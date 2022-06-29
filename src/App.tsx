7e0b47c42336c891000eb1a1e43a7d4bc3042064
Vo Hieu Thang <77570864+VOHIEUTHANG@users.noreply.github.com>
2022-06-29 12:34:46 +0700

    lesson 20 - restriting props

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,11 @@
 import React from "react";
 import "./App.css";
 import List from "./components/generics/List";
-
+import RandomNumber from "./components/restrict/RandomNumber";
 function App() {
   return (
     <div className="App">
-      <List
-        items={["Badman", "Spiderman", "Hulk"]}
-        handleClick={(name) => {
-          console.log(name);
-        }}
-      />
-      <List
-        items={[1, 2, 3]}
-        handleClick={(name) => {
-          console.log(name);
-        }}
-      />
-      <List
-        items={[{ name: "Thang" }, { name: "Vo" }, { name: "Hieu" }]}
-        handleClick={(name) => {
-          console.log(name);
-        }}
-      />
+      <RandomNumber value={10} isNegative={false} />
     </div>
   );
 }
---
+++ src/components/restrict/RandomNumber.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+type RandomNumberType = {
+  value: number;
+};
+type PositiveNumber = RandomNumberType & {
+  isPositive: boolean;
+  isNegative?: never;
+  isZero?: never;
+};
+type NegativeNumber = RandomNumberType & {
+  isPositive?: never;
+  isNegative: boolean;
+  isZero?: never;
+};
+type ZeroNumber = RandomNumberType & {
+  isPositive?: never;
+  isNegative?: never;
+  isZero: boolean;
+};
+
+type RandomNumberProps = PositiveNumber | NegativeNumber | ZeroNumber;
+
+const RandomNumber = ({
+  value,
+  isNegative,
+  isPositive,
+  isZero,
+}: RandomNumberProps) => {
+  return (
+    <div>
+      <h2>{value}</h2>
+      {isNegative && "Negative"}
+      {isPositive && "Positive"}
+      {isZero && "Zero"}
+    </div>
+  );
+};
+
+export default RandomNumber;
